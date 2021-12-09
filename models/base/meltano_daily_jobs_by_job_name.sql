c8c99d906158c0efb03d0e663caa82bcdd243e83
danielpdwalker <danielpdwalker@gmail.com>
2021-12-09 17:07:06 +0000

    Renamed job_name back to job_id to keep column name inline

--- models/base/meltano_daily_jobs_by_job_name.sql
+++ models/base/meltano_daily_jobs_by_job_name.sql
@@ -6,14 +6,14 @@ with meltano_jobs as (
 meltano_daily_jobs as (
     select
         started_at::date as date
-        , job_id as job_name
+        , job_id as job_id
         , count(*) as total_jobs
         , date_trunc('second', avg(job_duration)) as average_job_duration
         , sum(job_duration) as total_job_duration
         , sum(case when state = 'SUCCESS' then 1 else 0 end) as total_successful_jobs
         , sum(case when state = 'FAIL' then 1 else 0 end) as total_failed_jobs
     from meltano_jobs
-    group by job_name, started_at::date
+    group by job_id, started_at::date
     order by date desc
 )
 select * from meltano_daily_jobs

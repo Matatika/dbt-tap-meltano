383e2892632e632fdb599496d08a6db1c3c273e2
danielpdwalker <danielpdwalker@gmail.com>
2021-12-09 16:47:24 +0000

    Added new models and tests. Updated meltano_monthly_jobs to round the interval returned from avg(job_duration)

--- models/base/meltano_monthly_jobs_by_job_name.sql
+++ models/base/meltano_monthly_jobs_by_job_name.sql
@@ -6,13 +6,14 @@ with meltano_jobs as (
 meltano_monthly_jobs as (
     select
         to_char(started_at, 'YYYY-MM') as month
+        , job_id as job_name
         , count(*) as total_jobs
         , date_trunc('second', avg(job_duration)) as average_job_duration
         , sum(job_duration) as total_job_duration
         , sum(case when state = 'SUCCESS' then 1 else 0 end) as total_successful_jobs
         , sum(case when state = 'FAIL' then 1 else 0 end) as total_failed_jobs
     from meltano_jobs
-    group by to_char(started_at, 'YYYY-MM')
+    group by job_name, to_char(started_at, 'YYYY-MM')
     order by month desc
 )
 select * from meltano_monthly_jobs
---
+++ tests/assert_meltano_daily_jobs_by_job_name_count_equals_meltano_jobs.sql
@@ -0,0 +1,9 @@
+-- Assert that meltano_daily_jobs_by_job_name jobs count equals the total count of jobs from meltano_jobs for the last 30 days.
+
+select
+    sum(total_jobs)
+from {{ ref('meltano_daily_jobs_by_job_name')}}
+where date >= current_date - interval '5' day
+having sum(total_jobs) != (select count(*)
+                    from {{ ref('meltano_jobs')}}
+                    where started_at::date >= current_date - interval '5' day)
